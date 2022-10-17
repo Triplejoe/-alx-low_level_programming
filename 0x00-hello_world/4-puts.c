245c158847168c891d866ab72c9ba5a9eb5ccff6
Triplejoe <josephita65@gmail.com>
2022-10-17 14:15:46 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,9 @@
  * main - print a line of code using puts
  *
  *Return: Always 0 (success)
-
 */
-
-int main(void) 
-
-
+int main(void)
 {
 	puts("\"programming is like building a multilingul puzzle");
 	return (0);
-
 }
