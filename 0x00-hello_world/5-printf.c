fcc4440fec99f2215545ed7ebbe115c6d336d599
Triplejoe <josephita65@gmail.com>
2022-10-17 14:58:15 -0700

    task new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
-
-  main - Entry Point
-
-Return: Always 0 (success)
+* main - with proper grammar, but the outcome is a piece of art
+*Return: Always 0 (success)
 */
 int main(void)
-
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
