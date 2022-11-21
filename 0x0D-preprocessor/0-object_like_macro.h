81dd3330e7e2b2cfe658e2e25db4c824cbad30d7
Triplejoe <josephita65@gmail.com>
2022-11-20 19:12:09 -0800

    task

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
-#define ABS(X) ((X < (0)) ? ((X) * (-1)) : (X))
-#endif
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
